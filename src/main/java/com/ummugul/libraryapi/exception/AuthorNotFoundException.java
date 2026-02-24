e3c0ffdb782648643b3b5d9350078341b979facd
ummugulcaglar <ummugul.caglar@ogr.sakarya.edu.tr>
2026-02-24 13:19:13 +0300

    jwt'den sonra

--- src/main/java/com/ummugul/libraryapi/exception/AuthorNotFoundException.java
+++ src/main/java/com/ummugul/libraryapi/exception/AuthorNotFoundException.java
@@ -2,7 +2,8 @@ package com.ummugul.libraryapi.exception;
 
 public class AuthorNotFoundException extends RuntimeException{
 
-    public AuthorNotFoundException(String message){
+    public AuthorNotFoundException(String message)
+    {
             super(message);
     }
 }
--- src/main/java/com/ummugul/libraryapi/exception/BookNotFoundException.java
+++ src/main/java/com/ummugul/libraryapi/exception/BookNotFoundException.java
@@ -2,7 +2,7 @@ package com.ummugul.libraryapi.exception;
 
 public class BookNotFoundException extends RuntimeException{
 
-    public BookNotFoundException(String message){
-        super(message);
+    public BookNotFoundException(String messagee){
+        super(messagee);
     }
 }
